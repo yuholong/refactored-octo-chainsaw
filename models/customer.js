b9e0b5b85e2f548c5475f69aea972dcae2dabb40
Bryan <bryanyuhl@gmail.com>
2018-07-24 00:59:15 +0800

    ALT: add column 'id' to customer/pet models

--- models/customer.js
+++ models/customer.js
@@ -1,8 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Customer = sequelize.define('Customer', {
-    preference: DataTypes.STRING
-  }, {});
+  var Customer = sequelize.define(
+    'Customer',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      preference: DataTypes.STRING
+    },
+    {}
+  );
   Customer.associate = function(models) {
     // associations can be defined here
   };
--- models/pet.js
+++ models/pet.js
@@ -3,6 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   var Pet = sequelize.define(
     'Pet',
     {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
       name: DataTypes.STRING,
       available_from: DataTypes.DATE,
       age: DataTypes.INTEGER,
